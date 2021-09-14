930228c4c42f6e81fc335ad6799f720dcf9b90b1
Brianna Bias <brianna.bias@gmail.com>
2021-09-14 12:37:16 -0700

    form in request

--- src/components/request/Request.jsx
+++ src/components/request/Request.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Request = ({ }) => {
+const Request = ({ url, body, onChange, onSubmit  }) => {
     return (
         <>
+            <form onSubmit={onSubmit} className='request'>
+                <label htmlFor='url'></label>
+                <input
+                    aria-label='url'
+                    id='url'
+                    name='url'
+                    type='url'
+                    placeholder='https://'
+                    value={url}
+                    onChange={onChange}
+                />
+            </form>
         </>
     );
 };
